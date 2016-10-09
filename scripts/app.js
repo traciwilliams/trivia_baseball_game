7b75947797b87829e4d4b0211fa1902adb07f57d
Traci Williams <traciwilliams@Tracis-MacBook-Pro.local>
2016-10-08 20:22:04 -0500

    added the onclick functionality to start the getQuestion function

--- scripts/app.js
+++ scripts/app.js
@@ -4,17 +4,38 @@
 
 //==============GETTING QUESTION==================
 
-function getQuestion() {
+
+startButton = document.getElementById("beginGame");
+
+
+
+startButton.onclick = function getQuestion() {
+
     $.ajax({
-        url: 'http://www.omdbapi.com/?t=k-pax&y=&plot=short&r=json', //need to find a baseball API
-        type: 'Get',
-        dataType: 'json',
-        success: function (data) {
-            var questionDisplay = document.getElementById('question');
-            questionDisplay.textContent = (data.Plot);//change this one you figure out how the object displays information
-        },
-        error: function (err) {
-            console.log(err)
-        }
-    })
-}
+            url: 'http://www.omdbapi.com/?t=k-pax&y=&plot=short&r=json', //need to find a baseball API
+            type: 'Get',
+            dataType: 'json',
+            success: function (data) {
+                var questionDisplay = document.getElementById('question');
+                questionDisplay.textContent = (data.Plot);//change this one you figure out how the object displays information
+
+                //look at these...I think they need to be changed
+                //also would like a "pause" before these show up
+                var questionDisplay = document.getElementById('buttonNumberOne');
+                questionDisplay.textContent = (data.Year);
+
+                var questionDisplay = document.getElementById('buttonNumberTwo');
+                questionDisplay.textContent = (data.Title);
+
+                var questionDisplay = document.getElementById('buttonNumberThree');
+                questionDisplay.textContent = (data.Year);
+
+                var questionDisplay = document.getElementById('buttonNumberFour');
+                questionDisplay.textContent = (data.Title);
+
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        })
+    }
