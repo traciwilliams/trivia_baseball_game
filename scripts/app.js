c3e427d521155f10b187c72544a70d91c40352f2
Traci Williams <traciwilliams@Tracis-MacBook-Pro.local>
2016-10-09 20:20:17 -0500

    added questions

--- scripts/app.js
+++ scripts/app.js
@@ -1,45 +1,93 @@
 
+var question = {};
+    question.one = 'What was the score of the October 1, 1961 game in which Roger Maris hit his record-breaking 61st home run, breaking Babe Ruth’s 34-year-old single-season record?'
+    question.two = 'How many intentional walks did Roger Maris get in his 61 home run season?'
+    question.three = 'Name the first player born in the 1990s to appear in a major league game.'
+    question.four = 'Name the first AL expansion team to accomplish each of the following: First Division Title (1976), First League Pennant (1980), and World Series Champion (1985)'
+    question.five =  'Name the pitcher with the best won-lost percentage of any hurler with at least with 15 wins against the New York Yankees.'
+
+
+var answer = {};
+    answer.one = 'Yankees 1 … Red Sox 0';
+    answer.two = 'Zero';
+    answer.three = 'Cubs’ shortstop Starlin Castro';
+    answer.four = 'Kansas City Royals';
+    answer.five = 'Babe Ruth';
+
+window.onload = getQuestion();
+
+    function getQuestion (){
+
+
+        var questionDisplay = document.getElementById('question');
+        questionDisplay.textContent = (question.one);//change this one you figure out how the object displays information
+
+        var questionDisplay = document.getElementById('buttonNumberOne');
+        questionDisplay.textContent = (answer.one);
+
+        // var questionDisplay = document.getElementById('buttonNumberTwo');
+        // questionDisplay.textContent = (data.Title);
+        //
+        // var questionDisplay = document.getElementById('buttonNumberThree');
+        // questionDisplay.textContent = (data.Year);
+        //
+        // var questionDisplay = document.getElementById('buttonNumberFour');
+        // questionDisplay.textContent = (data.Title);
+    }
 
 
 
-//==============GETTING QUESTION==================
+
+//============QUIZ CONSTRUCTOR=========
+
+function quizConstructor(question, answer) {
+    this.question = question;
+    this.answer = answer;
+
+}
+
+
+
+
+
+//==============GETTING QUESTION USING AN API==================
 //might have to change this if I can't find an API and make my own array of questions
 //
 // startButton = document.getElementById("beginGame");
 // startButton.onclick = function getQuestion() {
-
-window.onload = getQuestion();
-
-function getQuestion() {
-
-    $.ajax({
-            url: 'http://www.omdbapi.com/?t=k-pax&y=&plot=short&r=json', //need to find a baseball API
-            type: 'Get',
-            dataType: 'json',
-            success: function (data) {
-                var questionDisplay = document.getElementById('question');
-                questionDisplay.textContent = (data.Plot);//change this one you figure out how the object displays information
-
-                //look at these...I think they need to be changed
-                //also would like a "pause" before these show up
-                var questionDisplay = document.getElementById('buttonNumberOne');
-                questionDisplay.textContent = (data.Year);
-
-                var questionDisplay = document.getElementById('buttonNumberTwo');
-                questionDisplay.textContent = (data.Title);
-
-                var questionDisplay = document.getElementById('buttonNumberThree');
-                questionDisplay.textContent = (data.Year);
-
-                var questionDisplay = document.getElementById('buttonNumberFour');
-                questionDisplay.textContent = (data.Title);
-
-            },
-            error: function (err) {
-                console.log(err)
-            }
-        })
-    }
+//
+// window.onload = getQuestion();
+//
+// function getQuestion() {
+//
+//     $.ajax({
+//             url: 'http://www.omdbapi.com/?t=k-pax&y=&plot=short&r=json', //need to find a baseball API
+//             type: 'Get',
+//             dataType: 'json',
+//             success: function (data) {
+//                 var questionDisplay = document.getElementById('question');
+//                 questionDisplay.textContent = (data.Plot);//change this one you figure out how the object displays information
+//
+//                 //look at these...I think they need to be changed
+//                 //also would like a "pause" before these show up
+//                 var questionDisplay = document.getElementById('buttonNumberOne');
+//                 questionDisplay.textContent = (data.Year);
+//
+//                 var questionDisplay = document.getElementById('buttonNumberTwo');
+//                 questionDisplay.textContent = (data.Title);
+//
+//                 var questionDisplay = document.getElementById('buttonNumberThree');
+//                 questionDisplay.textContent = (data.Year);
+//
+//                 var questionDisplay = document.getElementById('buttonNumberFour');
+//                 questionDisplay.textContent = (data.Title);
+//
+//             },
+//             error: function (err) {
+//                 console.log(err)
+//             }
+//         })
+//     }
 
 //==============TIMER==========
 //not finished...what happens when you get to one?
