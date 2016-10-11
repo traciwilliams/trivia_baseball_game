082af4a08226b8387b12c7d9eb73180112a61d6a
Traci Williams <traciwilliams@Tracis-MacBook-Pro.local>
2016-10-10 22:21:15 -0500

    added submit button and runs and outs but not working

--- scripts/app.js
+++ scripts/app.js
@@ -35,39 +35,34 @@
         // questionDisplay.textContent = (data.Title);
 //    }
 
+//TODOS
+//have to figure out how the constructor works and how to advance question
+//need a score function for runs and outs
+//reset timer
 
 
 
 //============QUIZ CONSTRUCTOR=========
-//
-// function Question(question, answer) {
-//     this.question = question;
-//     this.answer = answer;
-//
-// }
-// var firstQuestion = new Question ("What is baseball?", "a game");
-//
-
-
-
 
 window.onload = Question();
 
 
-     function Question(question, answer, choice1, choice2, choice3, choice4) {
+     function Question(question, answer, choice2, choice3, choice4) {
          this.question = question;
          this.answer = answer;
          this.choice2 = choice2;
          this.choice3 = choice3;
          this.choice4 = choice4;
 
+
+
      }
-     var firstQuestion = new Question ("Who was the last pitcher before Travis Wood to hit a home run in the postseason?", "Joe Blanton, PHI", "Mickey Mantel", "Yogi Berra", "Alex Rodriguez", "Ryne Sandburg");
+     var firstQuestion = new Question ("Who was the last pitcher before Travis Wood to hit a home run in the postseason?", "Joe Blanton", "Mickey Mantel", "Yogi Berra", "Alex Rodriguez", "Ryne Sandburg");
 
         question = document.getElementById('question');
         question.textContent = (firstQuestion.question);
 
-        answer = document.getElementById('choiceOne');
+        answer = document.getElementById('answer');
         answer.textContent = (firstQuestion.answer);
 
         choice2 = document.getElementById('choiceTwo');
@@ -82,41 +77,75 @@ window.onload = Question();
 
 
 //==============SUBMIT BUTTON and Calculation for Scoreboard ===========
+//this is not working...comeback to this
+
+        runs = 0;
+        outs = 0;
 
 
-submitButton = document.getElementById("submitAns");
-submitButton.onclick = function ifCorrect(){
-    //alert("are you working?")
+        submitButton = document.getElementById("submitAns");
+
+        submitButton.onclick = function ifCorrect(){
+            console.log('are you working? why yes I am!')
+            var correctAnswer = document.getElementById('answer');
+            console.log(correctAnswer)
 
 
+            if (correctAnswer.checked === true) {
+                alert('You are correct!')
+                runs = document.getElementById("runs").value;
+                runs += 1;
 
-}
+
+            } else {
+                alert('you are wrong')
+                outs = document.getElementById("outs").value;
+                outs += 1;
+            }
+        };
+
 
 
 
 //==============TIMER==========
-//not finished...what happens when you get to one?
 
-var count = 60;
+
+var count = 10;
 var counter= setInterval(timer, 1000); //1000 will  run it every 1 second
 
 function timer(){
     count=count-1;
-    if (count <= 0)
-    {
-        clearInterval(counter);
+    if (count < 0) {
         return;
     }
-    document.getElementById("timer").textContent = count + " secs";
+    document.getElementById("timer").textContent = count;
 };
 
 
+//===============RESET TIMER ===========
+//why do I need this? Come back to this
+// function resetTimer(){
+//     clearInterval(timer)
+//     timer = function timer(){
+//         count=count-1;
+//         if (count < 0) {
+//             return;
+//             }
+//         }
+// };
+//
+// resetTimer()
+resetButton = document.getElementById("resetButton");
+
+
+resetButton.click(function () {
+    clearTimer(timer);
+});
 
 
 
 
-
-//==============GETTING QUESTION USING AN API==================
+//==============GETTING QUESTIONS USING AN API==================
 //might have to change this if I can't find an API and make my own array of questions
 //
 // startButton = document.getElementById("beginGame");
